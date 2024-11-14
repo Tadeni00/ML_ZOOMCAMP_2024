340b0495ef46871d0b911801b0bfd1294f07c9c4
Tomisin Adeniyi <124089473+Tadeni00@users.noreply.github.com>
2024-11-14 12:46:50 +0000

    Web App

--- 05_deployment/client_score.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/predict"
-client = {"job": "student", "duration": 280, "poutcome": "failure"}
-
-response = requests.post(url, json=client)
-print(response.json())
-
-
--- 07_midterm_project/src/utils.py
+++
@@ -1,5 +0,0 @@
-# src/utils.py
-import pandas as pd
-
-def save_to_csv(df: pd.DataFrame, file_path: str):
-    df.to_csv(file_path, index=False)
